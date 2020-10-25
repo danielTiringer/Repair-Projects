2f66d06848998c977ed521eb5ccf380462510ce9
Daniel Tiringer <tiringerdaniel@gmail.com>
2020-10-25 14:54:56 +0100

    Update view to correct, add translations

--- views/project/view.php
+++ views/project/view.php
@@ -1,23 +1,48 @@
 <?php
 
 use yii\helpers\Html;
+use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Project */
 
-$this->title = Yii::t('app', 'Update Project: {name}', [
-    'name' => $model->id,
-]);
+$this->title = $model->make . ' ' . $model->model;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Projects'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
 ?>
-<div class="project-update">
+<div class="project-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
+    <p>
+        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
         'model' => $model,
+        'attributes' => [
+            'id',
+            'make',
+            'model',
+            'year',
+            'code',
+            'description:ntext',
+            'price',
+            'source',
+            'status',
+            'created_by',
+            'created_at:datetime',
+            'updated_by',
+            'updated_at:datetime',
+        ],
     ]) ?>
 
 </div>
