ada6b175f3ec4014bc239ddce8558da64b22558b
Daniel Tiringer <tiringerdaniel@gmail.com>
2020-11-09 21:01:06 +0100

    Collect the queries needed for tests

---
+++ tests/init-test.sql
@@ -0,0 +1,9 @@
+INSERT INTO `user`
+(`id`, `username`, `email`, `password`, `auth_key`, `access_token`)
+VALUES
+('100', 'admin', 'admin@example.com', '$2y$13$UiTaTFH5YxpGpuwqGbcZneWcbZeL/M53Mw4StOGBv62NGez9xYTIa', 'test100key', '100-token');
+
+INSERT INTO `user`
+(`id`, `username`, `email`, `password`, `auth_key`, `access_token`)
+VALUES
+('1', 'demo', 'demo@test.com', '$2y$13$/r5q0zm4v3Iu2qvRRUJG2eVeo8TKSSxHiDdskswm4OxGHgqL6CXJe', '', '');
