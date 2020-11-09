59ed205bd3ccf5286a3074ac02c09d8743829c61
Daniel Tiringer <tiringerdaniel@gmail.com>
2020-11-09 08:41:44 +0100

    Add projectimages alias

--- config/web.php
+++ config/web.php
@@ -11,6 +11,7 @@ $config = [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
+        '@projectImages' => 'uploads/project/'
     ],
     'language' => 'hu-HU',
     'sourceLanguage' => 'en-US',
