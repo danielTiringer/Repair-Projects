6082721953390d3e3462524b24208a3e39c8bb84
Daniel Tiringer <tiringerdaniel@gmail.com>
2020-11-09 21:36:54 +0100

    Write functional test to Project Index

---
+++ tests/functional/ProjectIndexCest.php
@@ -0,0 +1,35 @@
+<?php
+
+class ProjectIndexCest
+{
+    public function _before(\FunctionalTester $I)
+    {
+        $I->amLoggedInAs(\app\models\User::findByUsername('demo'));
+        $I->amOnPage(['project']);
+    }
+
+    public function cannotOpenProjectPageBeforeLogin(\FunctionalTester $I)
+    {
+        Yii::$app->user->logout();
+        $I->amOnPage(['project']);
+        $I->see(Yii::t('app', 'Login'), 'h1');
+    }
+
+    public function openProjectPage(\FunctionalTester $I)
+    {
+        $I->see(Yii::t('app', 'Projects'), 'h1');
+        $I->see(Yii::t('app', 'Create Project'), 'a');
+        $I->see(Yii::t('app', 'Make'), 'a');
+        $I->see(Yii::t('app', 'Model'), 'a');
+        $I->see(Yii::t('app', 'Description'), 'a');
+        $I->see(Yii::t('app', 'Year'), 'a');
+        $I->see(Yii::t('app', 'Source'), 'a');
+        $I->see(Yii::t('app', 'Status'), 'a');
+    }
+
+    public function navigateToNewProjectForm(\FunctionalTester $I)
+    {
+        $I->click(Yii::t('app', 'Create Project'));
+        $I->see(Yii::t('app', 'Create Project'), 'h1');
+    }
+}
