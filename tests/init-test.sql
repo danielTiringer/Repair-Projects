c93de85198601add4454a4083313139bd4ebb3e1
Daniel Tiringer <tiringerdaniel@gmail.com>
2020-12-28 22:23:58 +0100

    Add basic test data

--- tests/init-test.sql
+++ tests/init-test.sql
@@ -7,3 +7,13 @@ INSERT INTO `user`
 (`id`, `username`, `email`, `password`, `auth_key`, `access_token`)
 VALUES
 ('1', 'demo', 'demo@test.com', '$2y$13$/r5q0zm4v3Iu2qvRRUJG2eVeo8TKSSxHiDdskswm4OxGHgqL6CXJe', '', '');
+
+INSERT INTO `project`
+(`id`, `make`, `model`, `year`, `code`, `description`, `price`, `source`, `status`, `created_by`, `created_at`, `updated_by`, `updated_at`)
+VALUES
+('1', 'Test_Make', 'Test_Model', '2020', 'Test_Code', 'Test_Description', '100', '1', '1', '1', NULL, '1', NULL);
+
+INSERT INTO `status`
+(`id`, `status_description`)
+VALUES
+('1', 'Test_Status');
