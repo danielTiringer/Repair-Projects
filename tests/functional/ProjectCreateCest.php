321a638482d8a82da390b880901363b5998e7036
Daniel Tiringer <tiringerdaniel@gmail.com>
2020-12-28 22:24:33 +0100

    Test project creation functionality

--- tests/functional/ProjectCreateCest.php
+++ tests/functional/ProjectCreateCest.php
@@ -8,6 +8,19 @@ class ProjectCreateCest
         $I->amOnPage(['project/create']);
     }
 
+    public function openProjectCreatePage(\FunctionalTester $I)
+    {
+        $I->see(Yii::t('app', 'Create Project'), 'h1');
+        $I->see(Yii::t('app', 'Make'), 'label');
+        $I->see(Yii::t('app', 'Model'), 'label');
+        $I->see(Yii::t('app', 'Year'), 'label');
+        $I->see(Yii::t('app', 'Code'), 'label');
+        $I->see(Yii::t('app', 'Description'), 'label');
+        $I->see(Yii::t('app', 'Price'), 'label');
+        $I->see(Yii::t('app', 'Source'), 'label');
+        $I->see(Yii::t('app', 'Save'), 'button');
+    }
+
     public function createProjectWithoutNecessaryData(FunctionalTester $I)
     {
         $I->submitForm('#project-form', []);
@@ -16,5 +29,24 @@ class ProjectCreateCest
         $I->see(Yii::t('app', 'Model cannot be blank.'));
         $I->see(Yii::t('app', 'Year cannot be blank.'));
         $I->see(Yii::t('app', 'Price cannot be blank.'));
+    }
+
+    public function createProject(FunctionalTester $I)
+    {
+        $I->submitForm(
+            '#project-form',
+            [
+                'Project[make]' => 'Test Make',
+                'Project[model]' => 'Test Model',
+                'Project[year]' => 2020,
+                'Project[code]' => 'Test Code',
+                'Project[description]' => 'Just adding some test description here',
+                'Project[price]' => 5000,
+                'Project[source]' => 1,
+            ]
+        );
+        $I->dontSeeElement('form#project-form');
+        $I->seeRecord('app\models\Project', ['make' => 'Test Make', 'model' => 'Test Model']);
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
     }
 }
