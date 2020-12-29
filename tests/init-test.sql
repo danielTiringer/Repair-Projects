0820f42fec9f855423ed2e4719acdcf8235db173
Daniel Tiringer <tiringerdaniel@gmail.com>
2020-12-29 07:39:54 +0100

    Remove unnnecessary test data

--- tests/init-test.sql
+++ tests/init-test.sql
@@ -12,8 +12,3 @@ INSERT INTO `project`
 (`id`, `make`, `model`, `year`, `code`, `description`, `price`, `source`, `status`, `created_by`, `created_at`, `updated_by`, `updated_at`)
 VALUES
 ('1', 'Test_Make', 'Test_Model', '2020', 'Test_Code', 'Test_Description', '100', '1', '1', '1', NULL, '1', NULL);
-
-INSERT INTO `status`
-(`id`, `status_description`)
-VALUES
-('1', 'Test_Status');
